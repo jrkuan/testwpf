9159617ec7223eb3984716a75bc41077853b59d4
Jun Ren <jrkuan@gmail.com>
2017-04-28 23:28:54 +0800

    Initial Commit

---
+++ HelloWorld.Desktop/Bootstrapper.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Prism.Modularity;
+using HelloWorldModule;
+
+namespace HelloWorld.Desktop
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        /// <summary>
+        /// Create an instance of the shell window and return it
+        /// </summary>
+        /// <returns></returns>
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        /// <summary>
+        /// Display the shell to the user
+        /// </summary>
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        /// <summary>
+        /// Populate the module catalog with modules
+        /// </summary>
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(HelloWorldModule.HelloWorldModule));
+        }
+
+    }
+}
---
+++ HelloWorldModule/HelloWorldModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Prism;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace HelloWorldModule
+{
+    public class HelloWorldModule : IModule
+    {
+        private readonly IRegionManager regionManager;
+
+        public HelloWorldModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        /// <summary>
+        /// Registers a region name with its associated view type in the region view registry
+        /// </summary>
+        void IModule.Initialize()
+        {
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.HelloWorldView));
+
+        }
+    }
+}
