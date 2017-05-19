faf8a3ebe3de27acef83e100658be311903dcac0
JunRen <jrkuan@gmail.com>
2017-05-19 18:02:27 +0800

    Added singleton Core

---
+++ MinecraftModule/Services/Core.cs
@@ -0,0 +1,34 @@
+﻿namespace MinecraftModule.Services
+{
+    public sealed class Core
+    {
+        private static Core instance;
+
+        private static object threadlock = new object();
+
+        private Core()
+        {
+        }
+
+        public static Core Instance
+        {
+            get
+            {
+                lock (threadlock)
+                {
+                    if (instance == null)
+                    {
+                        return instance = new Core();
+                    }
+                    else
+                    {
+                        return instance;
+                    }
+                }          
+            }
+        }
+
+
+
+    }
+}
